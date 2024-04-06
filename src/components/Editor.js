f602c07d9e8be9f2e47943028ab368428717660b
Jeff St. Jean <jeffrey@stjean.ca>
2024-04-06 18:33:49 -0400

    Update colors

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from 'react';
-import ReactFlow, { Controls, Background, useNodesState, useEdgesState, addEdge, Position } from 'reactflow';
+import ReactFlow, { Controls, Background, useNodesState, useEdgesState, updateEdge, addEdge, Position, MarkerType } from 'reactflow';
 import 'reactflow/dist/style.css';
 
 import MenuBar from './MenuBar';
@@ -7,23 +7,41 @@ import TopTitlebar from './TopTitleBar';
 import SideMenuButton from './SideMenuButton';
 import SideToolbar from './SideToolBar';
 import SidePane from './SidePane';
+import { Constants } from '../Constants'
+
 import 'reactflow/dist/style.css';
 
-const DEFAULT_NODE_DIAMETER = 75;
-
-const nodeDefaults = {
+const NODE_DEFAULTS = {
     sourcePosition: Position.Right,
     targetPosition: Position.Left,
     style: {
       borderRadius: '100%',
-      backgroundColor: '#fff',
-      width: DEFAULT_NODE_DIAMETER,
-      height: DEFAULT_NODE_DIAMETER,
+      color: Constants.node.TEXT_COLOR,
+      backgroundColor: Constants.node.BACKGROUND_COLOR,
+      borderColor: Constants.node.STROKE_COLOR,
+      borderWidth: Constants.node.STROKE_WIDTH,
+      width: Constants.node.DEFAULT_DIAMETER,
+      height: Constants.node.DEFAULT_DIAMETER,
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
     },
 };
+
+const EDGE_DEFAULTS = {
+    type: Constants.EDGE_TYPE,
+    updatable: true,
+    markerEnd: {
+        type: MarkerType.ArrowClosed,
+        color: Constants.edge.STROKE_COLOR,
+        height: Constants.edge.ARROW_SIZE,
+        width: Constants.edge.ARROW_SIZE,
+    },
+    style: {
+        stroke: Constants.edge.STROKE_COLOR,
+        strokeWidth: Constants.edge.STROKE_WIDTH,
+    },
+}
 
 let id = 0;
 const getId = () => `q${id++}`;
@@ -32,11 +50,11 @@ const node1 = getId()
 const node2 = getId()
 
 const initialNodes = [
-    { id: node1, type: 'input', position: { x: -100, y: 0 }, data: { label: node1 }, ...nodeDefaults },
-    { id: node2, position: { x: 100, y: 0 }, data: { label: node2 }, ...nodeDefaults },
+    { id: node1, position: { x: -100, y: -30 }, data: { label: node1 }, ...NODE_DEFAULTS },
+    { id: node2, position: { x: 100, y: 30}, data: { label: node2 }, ...NODE_DEFAULTS },
 ];
 
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+const initialEdges = [{ id: 'e1-2', source: node1, target: node2, ...EDGE_DEFAULTS }];
 
 export default function Editor() {
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
@@ -46,10 +64,15 @@ export default function Editor() {
     // update edge array when connections happen
     const [isPaneOpen, setIsPaneOpen] = useState(false);
 
-    const onConnect = useCallback(
-        (params) => setEdges((eds) => addEdge(params, eds)),
-        [setEdges],
-    );
+    // on update of edges, modify the edge
+    const onEdgeUpdate = useCallback((oldEdge, newConnection) => {
+        setEdges((els) => updateEdge(oldEdge, newConnection, els))
+    }, [setEdges]);
+
+    // on new edges, add to edge array
+    const onConnect = useCallback((params) => {
+        setEdges((eds) => addEdge({ ...params, ...EDGE_DEFAULTS }, eds))
+    }, [setEdges]);
 
     // allow the 'move' drag effect on the create new node icon
     const onDragStart = (e) => {
@@ -66,17 +89,17 @@ export default function Editor() {
     const onDrop = useCallback(e => {
         e.preventDefault();
         const mousePosition = reactFlowInstance.screenToFlowPosition({ x: e.clientX, y: e.clientY });
-        const position =  {x: mousePosition.x-DEFAULT_NODE_DIAMETER/2, y: mousePosition.y-DEFAULT_NODE_DIAMETER/2 }
+        const position =  {x: mousePosition.x-Constants.node.DEFAULT_DIAMETER/2, y: mousePosition.y-Constants.node.DEFAULT_DIAMETER/2 }
         const id = getId();
         const newNode = {
             id: id,
             type: 'default',
             position,
             data: { label: id }, 
-            ...nodeDefaults
+            ...NODE_DEFAULTS
           };
           setNodes((nds) => nds.concat(newNode));
-    }, [nodes, setNodes, reactFlowInstance])
+    }, [setNodes, reactFlowInstance])
 
     return (
         <div style={{ display: 'flex', width: '100vw', height: '100vh' }}>
@@ -94,6 +117,7 @@ export default function Editor() {
                     edges={edges}
                     onNodesChange={onNodesChange}
                     onEdgesChange={onEdgesChange}
+                    onEdgeUpdate={onEdgeUpdate}
                     onConnect={onConnect}
                     onInit={setReactFlowInstance}
                     onDrop={onDrop}
